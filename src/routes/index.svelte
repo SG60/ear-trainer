3ca6ee9a028505e9a96bbefc5967583ff17b3246
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-03 21:04:56 +0000

    fix lighthouse meta info complaint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Ear Training Application" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
