047b8f8581e22b9b94d94d1e323dd673ef27b9e8
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-16 22:08:48 +0100

    svelte-migrate: updated files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,7 @@
-<script lang="ts" context="module">
-	throw new Error("@migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)");
-
-	// function getRandomInt(min: number, max: number) {
-	// 	min = Math.ceil(min);
-	// 	max = Math.floor(max);
-	// 	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-	// }
-</script>
-
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import * as Tone from 'tone';
+	import { getRandomInt } from './+page';
 
 	let synth: Tone.Synth<Tone.SynthOptions>;
 
