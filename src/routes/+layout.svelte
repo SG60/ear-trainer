4dc70baac78c665631e94be1daec4a208e3eb9ec
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-17 23:12:25 +0100

    @sentry/svelte tracing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,18 @@
+<script context="module">
+	import * as Sentry from '@sentry/svelte';
+	import { BrowserTracing } from '@sentry/tracing';
+
+	Sentry.init({
+		dsn: 'https://3ea9767fb4b945a98effa7c4b49d2ee3@o1154464.ingest.sentry.io/6661120',
+		integrations: [new BrowserTracing()],
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0
+	});
+</script>
+
 <script>
 	import '../app.css';
 </script>
