6bb96a1f13a57ef40076f0b55183785e42c88971
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-19 14:35:38 +0100

    pnpm format

--- src/routes/account/login/+page.ts
+++ src/routes/account/login/+page.ts
@@ -1,5 +1,5 @@
-import type {PageLoad} from './$types';
+import type { PageLoad } from './$types';
 
-export const load: PageLoad = ({data}) => {
-    console.log("data: ", data);
-}
+export const load: PageLoad = ({ data }) => {
+	console.log('data: ', data);
+};
