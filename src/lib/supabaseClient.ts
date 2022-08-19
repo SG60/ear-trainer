1b9a8d3de2701425f80ead9c9ffe4b460b349d8b
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-20 00:55:10 +0100

    Pass platform fetch to supabaseClient
    This should fix the errors that were happening on vercel edge.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,4 +4,6 @@ const supabaseUrl = 'https://weajrtbsntmezicrvolh.supabase.co';
 const supabaseAnonKey =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlYWpydGJzbnRtZXppY3J2b2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA4MzE5NTcsImV4cCI6MTk3NjQwNzk1N30.qeCt7aUoFq4Uj3gGqggSLDq9Ab9RgWyhwCgfj_Ku56k';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+	fetch: (...args) => fetch(...args)
+});
