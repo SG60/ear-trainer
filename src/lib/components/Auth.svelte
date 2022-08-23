ac489b27fa614fd74154b769a2eff09dac10a6d9
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-08-23 23:34:02 +0100

    always redirect to current url after authentication

--- src/lib/components/Auth.svelte
+++ src/lib/components/Auth.svelte
@@ -8,7 +8,10 @@
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const { error } = await supabase.auth.signIn({ email });
+			const { error } = await supabase.auth.signIn(
+				{ email },
+				{ redirectTo: window.location.origin }
+			);
 			if (error) throw error;
 			alert('Check your email for the login link!');
 			submitted = true;
