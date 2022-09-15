042c15d1361daf4048575016e536210c3fbe6dbe
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-09-15 18:01:59 +0100

    start sentry vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()],
-	build: { sourcemap: true }
-};
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit(), sentryUploadPlugin(env.PUBLIC_SENTRY_ENVIRONMENT)],
+		build: { sourcemap: true }
+	};
+});
 
-export default config;
+import { loadEnv } from 'vite';
+
+/** @type {(environment: string) => import('vite').Plugin} */
+function sentryUploadPlugin(sentryEnvironment) {
+	return {
+		name: 'sentry-upload',
+		apply: 'build',
+		writeBundle(options, bundle) {
+			// upload sourcemaps to sentry
+			console.log('uploading sourcemaps to sentry');
+			console.log(`environment: ${sentryEnvironment}`);
+		}
+	};
+}
