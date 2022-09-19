96a3172bc4b7eb30d2548a5716334a097df6ea6c
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-09-19 10:28:50 +0100

    some sentry release stuff kinda working (without actual upload)

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,39 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { loadEnv, defineConfig } from 'vite';
 import type { Plugin } from 'vite';
 
+import type { SentryCliOptions, SentryCliReleases } from '@sentry/cli';
+import SentryCli from '@sentry/cli';
+
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	return {
-		plugins: [sveltekit(), sentryUploadPlugin(env.PUBLIC_SENTRY_ENVIRONMENT)],
+		plugins: [
+			sveltekit(),
+			sentryUploadPlugin({ sentryEnvironment: env.PUBLIC_SENTRY_ENVIRONMENT })
+		],
 		build: { sourcemap: true }
 	};
 });
 
-function sentryUploadPlugin(sentryEnvironment: string): Plugin {
+interface ViteSentryPluginOptions extends SentryCliOptions /*, SentryCliReleases*/ {
+	sentryEnvironment: string;
+	release?: string;
+}
+
+async function sentryUploadPlugin(options: ViteSentryPluginOptions): Promise<Plugin> {
 	const virtualModuleId = 'virtual:sentry-upload';
 	const resolvedVirtualModuleId = '\0' + virtualModuleId;
+
+	/* Initialise new SentryCli instance */
+	const cli = new SentryCli(undefined, {
+		authToken: options.authToken,
+		org: options.org,
+		project: options.project,
+		url: options.url,
+		vcsRemote: options.vcsRemote
+	});
+
+	const currentRelease = await getReleasePromise(cli, options);
 
 	return {
 		name: 'sentry-upload',
@@ -27,21 +49,36 @@ function sentryUploadPlugin(sentryEnvironment: string): Plugin {
 		},
 		load(id) {
 			if (id === resolvedVirtualModuleId) {
-				return `export const release = "release from virtual module"`;
+				return `export const release = "${currentRelease}";`;
 			}
 		},
 
 		/* Sentry stuff */
-		closeBundle() {
-			const currentRelease = await getReleasePromise(cli, options);
-
-			// upload sourcemaps to sentry
-			console.log('uploading sourcemaps to sentry');
-			console.log(`environment: ${sentryEnvironment}`);
+		async closeBundle() {
+			if (!currentRelease) {
+				this.warn('No release found, skipping Sentry upload');
+			} else {
+				// upload sourcemaps to sentry
+				console.log('uploading sourcemaps to sentry (FAKE)');
+				console.log(`environment: ${options.sentryEnvironment}`);
+				console.log(`currentRelease: ${currentRelease}`);
+			}
 		}
 	};
 }
 
-async function getReleasePromise(cli, options) {
-	return null;
-}
+/* Prepare sentry release and returns promise */
+const getReleasePromise = (
+	cli: SentryCli,
+	options: {
+		/*
+		Unique name for release
+		defaults to sentry-cli releases propose version (requires access to GIT and root directory to be repo)
+		*/
+		release?: string;
+	} = {}
+) => {
+	return (options.release ? Promise.resolve(options.release) : cli.releases.proposeVersion())
+		.then((version: string) => `${version}`.trim())
+		.catch(() => undefined);
+};
