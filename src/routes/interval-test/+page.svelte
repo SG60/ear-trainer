29f0417f6ec181e6f3a50395dc313986678d9b6d
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-10-14 01:36:02 +0100

    supabase cli initial setup, also made some notes about the function for storing question and answer results in postgresql

--- src/routes/interval-test/+page.svelte
+++ src/routes/interval-test/+page.svelte
@@ -73,6 +73,39 @@
 			return 'success';
 		}
 
+		/*
+Combined SQL function to do submissions in one transaction.
+
+CREATE TYPE answer_information as (
+  question_data jsonb,
+  correct boolean,
+  question_type int8
+);
+
+CREATE OR REPLACE FUNCTION submit_question(user_id uuid, is_public boolean, answer_data answer_information[])
+returns void                                                                                                  
+language plpgsql
+as $$
+declare
+  game_played_id int8;
+begin
+  insert into games_played("user_id","is_public") VALUES(submit_question.user_id,submit_question.is_public) RETURNING id INTO game
+_played_id;
+  INSERT INTO answers("game_played_id","user_id","question_data", "correct", "question_type")
+    SELECT * FROM unnest(array_fill(game_played_id,ARRAY[array_length(submit_question.answer_data,1)]),array_fill(submit_question.
+user_id,ARRAY[array_length(submit_question.answer_data,1)]),submit_question.answer_data);
+end;
+$$;
+
+
+Using the function:
+
+SELECT submit_question('d5163409-b6b4-465f-bf96-2765c42f45c3'::uuid, true, ARRAY[
+(jsonb_object('{{thingy,5},{ahh,1}}'),true,1), (jsonb_object('{thingy,200}'),false,1)]::answer_information[]);
+
+
+		*/
+
 		const userId = $user.id;
 		const { data, error } = (await supabase.from('games_played').insert({
 			user_id: userId,
