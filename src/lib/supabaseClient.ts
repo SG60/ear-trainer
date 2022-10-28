e0d9a43682996468edeca35caff08239fe8211dc
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-10-28 16:06:43 +0100

    Improve database types for answer submission

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -6,7 +6,25 @@ const supabaseUrl = 'https://weajrtbsntmezicrvolh.supabase.co';
 const supabaseAnonKey =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlYWpydGJzbnRtZXppY3J2b2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA4MzE5NTcsImV4cCI6MTk3NjQwNzk1N30.qeCt7aUoFq4Uj3gGqggSLDq9Ab9RgWyhwCgfj_Ku56k';
 
-/** Client-side supabase client */
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+/** Client-side supabase client *****************************************/
+export const supabase = createClient<DBWithCustomTypes>(supabaseUrl, supabaseAnonKey, {
 	global: { fetch: (...args) => fetch(...args) }
 });
+
+/********************* Improved DB Types ******************************/
+interface IndividualAnswer {
+	question_data: {
+		interval: number;
+	};
+	correct: boolean;
+	question_type: QuestionType;
+}
+type SubmitQuestionAnswerData = IndividualAnswer[];
+export enum QuestionType {
+	FunctionalIntervals = 1,
+	VerticalIntervals = 2
+}
+
+export type DBWithCustomTypes = Database & {
+	public: { Functions: { submit_question: { Args: { answer_data: SubmitQuestionAnswerData } } } };
+};
