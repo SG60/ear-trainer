0b60e990acef7ba1cebbea8a107d72f37af2e64b
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-10-28 16:14:20 +0100

    New Sentry component tracking config method

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
-import * as Sentry from '@sentry/svelte';
+import { withSentryConfig } from '@sentry/svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,20 +10,6 @@ const config = {
 		preprocess({
 			postcss: true,
 			sourceMap: true
-		}),
-		Sentry.componentTrackingPreprocessor({
-			// Add the components you want to be tracked to this array.
-			// Specify `true` to track all components or `false` to disable
-			// tracking entirely
-			// (defaults to `true`)
-			// trackComponents: ['Navbar', 'PrimaryButton', 'LoginForm'],
-			trackComponents: true,
-			// To disable component initialization spans, set this to `false`.
-			// (defaults to `true`)
-			trackInit: true,
-			// To disable component update spans, set this to `false`
-			// (defaults to `true`)
-			trackUpdates: false
 		})
 	],
 
@@ -39,4 +25,4 @@ const config = {
 	}
 };
 
-export default config;
+export default withSentryConfig(config);
